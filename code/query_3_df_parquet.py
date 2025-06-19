3248885b921e0563739b1bc5109f464dbaff09a6
Georgiannarenta <georgianna.renta13@gmail.com>
2025-06-19 18:03:19 +0300

    Update query_3_df_parquet.py

--- code/query_3_df_parquet.py
+++ code/query_3_df_parquet.py
@@ -1,15 +1,16 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, round, regexp_replace
 
-username = "username"  # π.χ. georgiannarenta, ioannisanagnostaras
+username = "username"  #  georgiannarenta, ioannisanagnostaras
 
 spark = SparkSession.builder.appName("Query3_DataFrame_RegexClean").getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel("ERROR")
 
 job_id = sc.applicationId
-output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query3_df_output_{job_id}"
+output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query3_df_final_output_{job_id}"
 
+# Ανάγνωση δεδομένων από parquet
 pop_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/2010_Census_Populations_by_Zip_Code.parquet")
 income_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_income_2015.parquet")
 
@@ -25,9 +26,9 @@ pop_df = pop_df.withColumn("Zip Code", col("Zip Code").cast("string")) \
 
 income_clean_df = income_clean_df.withColumn("Zip Code", col("Zip Code").cast("string"))
 
-# Φιλτράρισμα τιμών > 0
-pop_filtered_df = pop_df.filter(col("Average Household Size") > 0)
-income_filtered_df = income_clean_df.filter(col("Estimated_Median_Income_Clean") > 0)
+# Φιλτράρισμα για μη-null τιμές και τιμές μεγαλύτερες του 0
+pop_filtered_df = pop_df.filter(col("Average Household Size").isNotNull() & (col("Average Household Size") > 0))
+income_filtered_df = income_clean_df.filter(col("Estimated_Median_Income_Clean").isNotNull() & (col("Estimated_Median_Income_Clean") > 0))
 
 # Join
 joined_df = pop_filtered_df.alias("p").join(
@@ -48,7 +49,14 @@ result_df = joined_df.withColumn(
     col("Income_per_Person")
 )
 
-result_df.show()
+# Collect και εκτύπωση των αποτελεσμάτων
+result = result_df.collect()
 
+# Εκτύπωση των αποτελεσμάτων
+for item in result:
+    print(item)
+
+# Αποθήκευση στο HDFS
 result_df.write.mode("overwrite").parquet(output_dir)
+
 
