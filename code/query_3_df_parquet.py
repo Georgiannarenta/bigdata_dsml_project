d6a6f4c7cf8f3d003c99770ae95d605a66b2dc54
Georgiannarenta <georgianna.renta13@gmail.com>
2025-06-19 16:42:26 +0300

    Update query_3_df_parquet.py

--- code/query_3_df_parquet.py
+++ code/query_3_df_parquet.py
@@ -1,9 +1,9 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, round
+from pyspark.sql.functions import col, round, regexp_replace
 
-username = "username" #georgiannarenta,ioannisanagnostaras
+username = "username"  # georgiannarenta, ioannisanagnostaras
 
-spark = SparkSession.builder.appName("Query3_DataFrame").getOrCreate()
+spark = SparkSession.builder.appName("Query3_DataFrame_FilterDollar").getOrCreate()
 sc = spark.sparkContext
 sc.setLogLevel("ERROR")
 
@@ -14,33 +14,39 @@ output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query3_df_output_{job_i
 pop_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/2010_Census_Populations_by_Zip_Code.parquet")
 income_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_income_2015.parquet")
 
+# Φιλτράρισμα εισοδήματος που ξεκινάει με $ (π.χ. "$50,000")
+income_filtered_df = income_df.filter(col("Estimated Median Income").rlike(r"^\$"))
+
+# Καθαρισμός της στήλης Estimated Median Income (remove $ και ,)
+income_clean_df = income_filtered_df.withColumn(
+    "Estimated_Median_Income_Clean",
+    regexp_replace(regexp_replace(col("Estimated Median Income"), "\\$", ""), ",", "").cast("double")
+)
+
 # Join στα Zip Code
 joined_df = pop_df.alias("p").join(
-    income_df.alias("i"),
+    income_clean_df.alias("i"),
     col("p.Zip Code") == col("i.Zip Code")
 ).select(
     col("p.Zip Code"),
     col("p.Average Household Size"),
-    col("i.Estimated Median Income")
+    col("i.Estimated_Median_Income_Clean").alias("Estimated Median Income")
 )
 
+# Φιλτράρισμα μόνο με Average Household Size != 0 (χωρίς null check όπως ζητήθηκε)
+filtered_df = joined_df.filter(col("Average Household Size") != 0)
+
 # Υπολογισμός εισοδήματος ανά άτομο
-result_df = joined_df.filter(
-    (col("Average Household Size").isNotNull()) & (col("Average Household Size") != 0) &
-    (col("Estimated Median Income").isNotNull())
-).withColumn(
+result_df = filtered_df.withColumn(
     "Income_per_Person",
     round(col("Estimated Median Income") / col("Average Household Size"), 6)
 ).select(
     col("Zip Code"),
     col("Income_per_Person")
-)
-
-# Ταξινόμηση (προαιρετικό)
-result_df = result_df.orderBy(col("Income_per_Person").desc())
-
+).orderBy(col("Income_per_Person").desc())
 
 result_df.show()
 
 # Αποθήκευση αποτελεσμάτων
 result_df.write.mode("overwrite").parquet(output_dir)
+
