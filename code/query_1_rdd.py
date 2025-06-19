3634437e95588eb0474075333b51606f9cb4331a
IoannisAnagnostaras <anagnostarasjohndi@gmail.com>
2025-06-19 12:13:56 +0300

    Update query_1_rdd.py

--- code/query_1_rdd.py
+++ code/query_1_rdd.py
@@ -4,10 +4,11 @@ username = "username" #georgiannarenta #ioannisanagnostaras
 spark = SparkSession.builder.appName("Query 1 RDD").getOrCreate()                        
 sc.setLogLevel("ERROR") 
 job_id = sc.applicationId 
-output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query1_rdd_output_{job_id}"                                                                                                                                                            crime_data_10_19 = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_Crime_Data_2010_2019.>crime_data_20_25 = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_Crime_Data_2020_2025.>
+output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query1_rdd_output_{job_id}"   
+crime_data_10_19 = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_Crime_Data_2010_2019").rdd
+crime_data_20_25 = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_Crime_Data_2020_2025").rdd
 crime_data_10_19_filtered = crime_data_10_19.map(lambda x: (x['Crm Cd Desc'], x['Vict Age']))
 crime_data_20_25_filtered = crime_data_20_25.map(lambda x: (x['Crm Cd Desc'], x['Vict Age']))
-
 crime_data_rdd = crime_data_10_19_filtered.union(crime_data_20_25_filtered)
 
 filtered = crime_data_rdd.filter(lambda x: x[0] and "AGGRAVATED ASSAULT" in x[0] and x[1])
