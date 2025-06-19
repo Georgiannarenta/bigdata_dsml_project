aa4981148e6bf5e934b4f0b8df988d5155ab7f66
Georgiannarenta <georgianna.renta13@gmail.com>
2025-06-19 17:09:29 +0300

    Create query_3_df_csv.py

---
+++ code/query_3_df_csv.py
@@ -0,0 +1,51 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, round, regexp_replace
+
+
+username = "username"  # georgiannarenta, ioannisanagnostaras
+
+
+spark = SparkSession.builder.appName("Query3_DataFrame_FilterDollar").getOrCreate()
+sc = spark.sparkContext
+sc.setLogLevel("ERROR")
+
+
+job_id = sc.applicationId
+output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query3_df_output_{job_id}_csv"
+
+
+pop_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/2010_Census_Populations_by_Zip_Code.parquet")
+income_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_income_2015.parquet")
+
+# Καθαρισμός του πεδίου εισοδήματος: αφαίρεση $, , και cast σε double
+income_clean_df = income_df.withColumn(
+    "Estimated_Median_Income_Clean",
+    regexp_replace(regexp_replace(col("Estimated Median Income"), "\\$", ""), ",", "").cast("double")
+)
+
+
+joined_df = pop_df.alias("p").join(
+    income_clean_df.alias("i"),
+    col("p.Zip Code") == col("i.Zip Code")
+).select(
+    col("p.Zip Code"),
+    col("p.Average Household Size"),
+    col("i.Estimated_Median_Income_Clean").alias("Estimated Median Income")
+)
+
+result_df = joined_df.withColumn(
+    "Income_per_Person",
+    round(col("Estimated Median Income") / col("Average Household Size"), 6)
+).select(
+    col("Zip Code"),
+    col("Income_per_Person")
+)
+
+
+result_df.show()
+
+
+result_df.write \
+    .mode("overwrite") \
+    .option("header", "true") \
+    .csv(output_dir)
