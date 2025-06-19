3d26bd9b298b45e6201fd45d7cfc16d3fd5c6beb
IoannisAnagnostaras <anagnostarasjohndi@gmail.com>
2025-06-19 18:51:57 +0300

    Update query_3_df_parquet.py

--- code/query_3_df_parquet.py
+++ code/query_3_df_parquet.py
@@ -10,27 +10,23 @@ sc.setLogLevel("ERROR")
 job_id = sc.applicationId
 output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query3_df_final_output_{job_id}"
 
-# Ανάγνωση δεδομένων από parquet
 pop_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/2010_Census_Populations_by_Zip_Code.parquet")
 income_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_income_2015.parquet")
 
-# Καθαρισμός εισοδήματος με regexp_replace για αφαίρεση $ και , και cast σε double
 income_clean_df = income_df.withColumn(
     "Estimated_Median_Income_Clean",
     regexp_replace(regexp_replace(col("Estimated Median Income"), "\\$", ""), ",", "").cast("double")
 )
 
-# Μετατροπή Zip Code σε string και cast του Average Household Size σε double
 pop_df = pop_df.withColumn("Zip Code", col("Zip Code").cast("string")) \
                .withColumn("Average Household Size", col("Average Household Size").cast("double"))
 
 income_clean_df = income_clean_df.withColumn("Zip Code", col("Zip Code").cast("string"))
 
-# Φιλτράρισμα για μη-null τιμές και τιμές μεγαλύτερες του 0
 pop_filtered_df = pop_df.filter(col("Average Household Size").isNotNull() & (col("Average Household Size") > 0))
 income_filtered_df = income_clean_df.filter(col("Estimated_Median_Income_Clean").isNotNull() & (col("Estimated_Median_Income_Clean") > 0))
 
-# Join
+
 joined_df = pop_filtered_df.alias("p").join(
     income_filtered_df.alias("i"),
     col("p.Zip Code") == col("i.Zip Code")
@@ -40,7 +36,6 @@ joined_df = pop_filtered_df.alias("p").join(
     col("i.Estimated_Median_Income_Clean").alias("Estimated Median Income")
 )
 
-# Υπολογισμός εισοδήματος ανά άτομο
 result_df = joined_df.withColumn(
     "Income_per_Person",
     round(col("Estimated Median Income") / col("Average Household Size"), 6)
@@ -49,10 +44,9 @@ result_df = joined_df.withColumn(
     col("Income_per_Person")
 )
 
-# Collect και εκτύπωση των αποτελεσμάτων
+
 result = result_df.collect()
 
-# Εκτύπωση των αποτελεσμάτων
 for item in result:
     print(item)
 
