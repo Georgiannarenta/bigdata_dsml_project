94bdf7b5edb288e118890b9766337e33e4ebf477
IoannisAnagnostaras <anagnostarasjohndi@gmail.com>
2025-06-20 15:53:58 +0300

    Create query_4_2_4_8.py

---
+++ code/query_4_2_4_8.py
@@ -0,0 +1,91 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import split, explode, col, lower, upper, udf, sqrt, pow, count, avg
+from pyspark.sql.window import Window
+from pyspark.sql.functions import row_number
+from pyspark.sql.types import DoubleType
+import math 
+
+username = 'username' #georgiannarenta, ioannisanagnostaras
+spark = (
+    SparkSession.builder
+    .appName("Query4_2_4_8")
+    .config("spark.executor.instances", "2") 
+    .config("spark.executor.cores", "4")     
+    .config("spark.executor.memory", "8g")  
+    .getOrCreate()
+)
+sc = spark.sparkContext
+sc.setLogLevel("ERROR")
+
+job_id = sc.applicationId
+output_dir = f"hdfs://hdfs-namenode:9000/user/{username}/query4_df_2_4_8_output_{job_id}"
+crime_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_Crime_Data_2010_2019.parquet") \
+           .union(spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_Crime_Data_2020_2025.parquet"))
+
+police_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/LA_Police_Stations.parquet")
+mo_codes_df = spark.read.parquet(f"hdfs://hdfs-namenode:9000/user/{username}/data/parquet/MO_codes.parquet")
+
+crime_df = crime_df.withColumn("mo_code", explode(split(col("Mocodes"), " "))).filter(col("mo_code") != "")
+
+
+crime_df = crime_df.filter((col("LAT") != 0) & (col("LON") != 0))
+
+
+mo_codes_filtered = mo_codes_df.filter(
+    (lower(col("description")).contains("gun")) | (lower(col("description")).contains("weapon"))
+).select("code")
+
+crime_weapons = crime_df.join(mo_codes_filtered, crime_df.mo_code == mo_codes_filtered.code, "inner")
+
+R = 6371000  # ακτίνα γης σε μέτρα
+lat0_rad = math.radians(34.0)  # Κεντρικό latitude για LA σε ακτίνια
+
+def transform_x(lon, lat):
+    if lon is None or lat is None:
+        return None
+    lon_rad = math.radians(lon)
+    return R * lon_rad * math.cos(lat0_rad)
+
+def transform_y(lon, lat):
+    if lon is None or lat is None:
+        return None
+    lat_rad = math.radians(lat)
+    return R * lat_rad
+
+x_udf = udf(transform_x, DoubleType())
+y_udf = udf(transform_y, DoubleType())
+
+
+crime_weapons = crime_weapons.withColumn("crime_x", x_udf(col("LON"), col("LAT"))) \
+                             .withColumn("crime_y", y_udf(col("LON"), col("LAT"))) \
+                             .withColumn("AREA_NAME_UPPER", upper(col("AREA NAME")))
+
+police_df = police_df.withColumn("DIVISION_UPPER", upper(col("DIVISION")))
+
+
+joined_df = crime_weapons.join(police_df, crime_weapons.AREA_NAME_UPPER == police_df.DIVISION_UPPER, "inner")
+
+joined_df = joined_df.withColumn(
+    "distance",
+    sqrt(
+        pow(col("crime_x") - col("X"), 2) +
+        pow(col("crime_y") - col("Y"), 2)
+    )
+)
+
+
+part = Window.partitionBy("crime_id").orderBy(col("distance").asc())
+
+closest_df = joined_df.withColumn("rn", row_number().over(part)).filter(col("rn") == 1)
+
+result = closest_df.groupBy("DIVISION_UPPER") \
+                   .agg(
+                       count("*").alias("#"),
+                       avg("distance").alias("average_distance")
+                   ) \
+                   .orderBy(col("#").desc())
+
+
+result.show(truncate=False)
+result.write.mode("overwrite").parquet(output_dir)
+
